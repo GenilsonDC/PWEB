f06d20aa1ff0df0f49bd2af53fd78aaa0dc2d5d2
Genilson do Carmo <57676824+GenilsonDC@users.noreply.github.com>
2023-05-19 23:20:39 -0300

    🏧 by Function
    Criando  Function para tipos de conta bancaria simples

---
+++ Atividade.11/Retangulo.js
@@ -0,0 +1,9 @@
+var base = prompt("✏️ Digite o valor da base: ");
+var altura = prompt("✏️ Digite o valor da altura: ");
+var retangulo = {};
+
+retangulo.altura = altura;
+retangulo.base = base;
+var area = `📐 Área do retangulo= ${retangulo.base * retangulo.altura}M2`;
+
+var any = document.querySelector().value;
