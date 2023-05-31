d9414084a3cbef5dd2f395b6f65a0e2620bd7b66
Genilson do Carmo <57676824+GenilsonDC@users.noreply.github.com>
2023-05-31 16:21:55 -0300

    🟢 Node teste em MAC OS e W
    Testando o node nos diferentes SOs

--- Aula_Node/primeiro.js
+++ Aula_Node/primeiro.js
@@ -1,3 +1,3 @@
-/* Comando para rodar o programa: npm nomePrograma.exteçao 
+/* Comando para rodar o programa: node nomePrograma.exteçao 
  e se tiver argumentos nomePrograma.exteçao 1 2 3 4 ...*/
 console.log("\t\nPrimeiro exemplo\n");
